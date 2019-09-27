4b3d91550961bb50347c13dbe5f299f34b738833
Pavel_Varabyou <Pavel.Varabyou@artezio.com>
2019-09-27 12:11:29 +0300

    TernarySearch

--- src/main/java/by/pepega/algorithm/AlgorithmApplication.java
+++ src/main/java/by/pepega/algorithm/AlgorithmApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AlgorithmApplication {
 
     public static void main(String[] args) {
-//		SpringApplication.run(AlgorithmApplication.class, args);
+//        SpringApplication.run(AlgorithmApplication.class, args);
     }
 
 }
--- src/main/java/by/pepega/algorithm/algorithms/search/TernarySearch.java
+++ src/main/java/by/pepega/algorithm/algorithms/search/TernarySearch.java
@@ -1,8 +1,10 @@
 package by.pepega.algorithm.algorithms.search;
 
-import by.pepega.algorithm.structures.Array;
+import java.util.function.Function;
 
-public class TernarySearch<T extends Comparable<T>> implements SearchAlgorithm<T> {
+import by.pepega.algorithm.algorithms.Algorithm;
+
+public class TernarySearch implements Algorithm {
 
     @Override
     public String getName() {
@@ -10,9 +12,29 @@ public class TernarySearch<T extends Comparable<T>> implements SearchAlgorithm<T
     }
 
 
-    @Override
-    public int search(Array<T> elements, T element) {
-        return -1;
+    public double searchMin(Function<Double, Double> func, double left, double right, double accuracy) {
+        if (right - left < accuracy) {
+            return (left + right) / 2;
+        }
+
+        double midLeft = left + (right - left) / 3;
+        double midRight = left + 2 * (right - left) / 3;
+
+        return func.apply(midLeft) <= func.apply(midRight)
+            ? searchMin(func, left, midRight, accuracy)
+            : searchMin(func, midLeft, right, accuracy);
     }
 
+    public double searchMax(Function<Double, Double> func, double left, double right, double accuracy) {
+        while (right - left >= accuracy) {
+            double midLeft = left + (right - left) / 3;
+            double midRight = left + 2 * (right - left) / 3;
+            if (func.apply(midLeft) > func.apply(midRight)) {
+                right = midRight;
+            } else {
+                left = midLeft;
+            }
+        }
+        return (left + right) / 2;
+    }
 }
